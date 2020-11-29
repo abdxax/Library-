3ca0e1ad5e4987c0bf89d0febc270393c8968d0f
abdxax <abdxax>
2020-11-29 22:04:10 +0300

    fin7

--- besh_last2.sql
+++ besh_last2.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: 12 نوفمبر 2020 الساعة 23:37
--- إصدار الخادم: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Host: 127.0.0.1
+-- Generation Time: 29 نوفمبر 2020 الساعة 20:03
+-- إصدار الخادم: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,8 +33,26 @@ CREATE TABLE `bill` (
   `total` varchar(200) NOT NULL,
   `payway` varchar(200) NOT NULL,
   `residual` varchar(40) NOT NULL,
-  `email` varchar(200) NOT NULL
+  `email` varchar(200) NOT NULL,
+  `totalpay` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `bill`
+--
+
+INSERT INTO `bill` (`id`, `total`, `payway`, `residual`, `email`, `totalpay`, `status`) VALUES
+(1, '308', '1', '0', '20111', 0, ''),
+(2, '308', '1', '0', '20111', 0, ''),
+(3, '308', '1', '0', '20111', 0, ''),
+(4, '308', '1', '0', '20111', 0, ''),
+(5, '308', '1', '0', '20111', 0, ''),
+(6, '308', '1', '0', '20111', 0, ''),
+(7, '88', '2', '0', '20111', 0, ''),
+(8, '168', '2', '0', '20111', 0, 'done'),
+(9, '50', '2', '0', '20111', 50, 'done'),
+(10, '50', '1', '0', '20111', 0, 'done');
 
 -- --------------------------------------------------------
 
@@ -44,8 +63,48 @@ CREATE TABLE `bill` (
 CREATE TABLE `car` (
   `email` varchar(200) NOT NULL,
   `contact_id` int(11) NOT NULL,
-  `qu` int(11) NOT NULL
+  `qu` int(11) NOT NULL,
+  `bill_id` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `car`
+--
+
+INSERT INTO `car` (`email`, `contact_id`, `qu`, `bill_id`, `status`) VALUES
+('20111', 3, 1, 0, 'wait'),
+('20111', 4, 1, 0, 'wait'),
+('20111', 3, 3, 0, 'wait'),
+('20111', 2, 1, 0, 'wait'),
+('20111', 2, 1, 0, 'wait'),
+('20111', 2, 1, 0, 'wait'),
+('20111', 3, 1, 0, 'wait'),
+('20111', 3, 1, 7, 'wait'),
+('20111', 3, 1, 8, 'done'),
+('20111', 2, 1, 8, 'done'),
+('20111', 4, 1, 8, 'done'),
+('20111', 2, 1, 9, 'done'),
+('20111', 2, 1, 10, 'done');
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `colloeg_dep`
+--
+
+CREATE TABLE `colloeg_dep` (
+  `id` int(11) NOT NULL,
+  `name_col` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `colloeg_dep`
+--
+
+INSERT INTO `colloeg_dep` (`id`, `name_col`) VALUES
+(1, 'الهندسة '),
+(2, 'الحاسب');
 
 -- --------------------------------------------------------
 
@@ -61,6 +120,15 @@ CREATE TABLE `contact` (
   `quenity` int(11) NOT NULL,
   `file_path` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `contact`
+--
+
+INSERT INTO `contact` (`id`, `id_types`, `title`, `price`, `quenity`, `file_path`) VALUES
+(2, 3, 'كتاب 1', '50', 20, '../poster/1qkbYI7WgR0wATRmb2lKQ5qqPagF2Jnhymcy2OzS.jpeg'),
+(3, 3, 'كتاب  2', '30', 6, '../poster/University Emblem v1.jpg'),
+(4, 3, 'كتاب 2', '88', 9, '../poster/بدون عنوان.jpg');
 
 -- --------------------------------------------------------
 
@@ -79,7 +147,7 @@ CREATE TABLE `departemnt` (
 
 INSERT INTO `departemnt` (`id`, `dep_name`) VALUES
 (1, 'قرطاسية'),
-(2, 'قرطاسية٩');
+(3, 'كتب');
 
 -- --------------------------------------------------------
 
@@ -91,15 +159,43 @@ CREATE TABLE `info` (
   `user_id` varchar(200) NOT NULL,
   `name` varchar(200) NOT NULL,
   `phone` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL
+  `email` varchar(200) NOT NULL,
+  `college` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- إرجاع أو استيراد بيانات الجدول `info`
 --
 
-INSERT INTO `info` (`user_id`, `name`, `phone`, `email`) VALUES
-('1111', 'eee', '22222', 'info@neamaah.com');
+INSERT INTO `info` (`user_id`, `name`, `phone`, `email`, `college`) VALUES
+('1111', 'eee', '22222', 'info@neamaah.com', 0),
+('1212', '1212', '+966568508989', 'aljarallahabdulrahman1212@gmail.com', 1),
+('20111', 'عبدالرحمن', '+966568508989', 'aa@a.aa', 1),
+('2222', 'Abdulrahman ALJarallah', '+966568508989', 'aljarallahabdulrahman@gmail.com', 0),
+('88809', 'Abdulrahman ALJarallah', '+966568508989', 'aljarallahabdulrahman@gmail.com', 1);
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `pdf`
+--
+
+CREATE TABLE `pdf` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cols_id` int(11) NOT NULL,
+  `file_path` text NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `descrip` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `pdf`
+--
+
+INSERT INTO `pdf` (`id`, `user_id`, `cols_id`, `file_path`, `title`, `descrip`) VALUES
+(2, 1, 1, '1111', 'ss', 'ww'),
+(3, 1212, 1, '../pdfs/Attachment_0.pdf', 'Javav', 'Java 1');
 
 -- --------------------------------------------------------
 
@@ -150,7 +246,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `userName`, `password`, `role_i`) VALUES
-(3, '1111', '011c945f30ce2cbafc452f39840f025693339c42', 2);
+(3, '1111', '011c945f30ce2cbafc452f39840f025693339c42', 2),
+(4, '2222', '7c4a8d09ca3762af61e59520943dc26494f8941b', 2),
+(5, '20111', '7c4a8d09ca3762af61e59520943dc26494f8941b', 2),
+(6, '9990', '7c4a8d09ca3762af61e59520943dc26494f8941b', 3),
+(7, '88809', '7c4a8d09ca3762af61e59520943dc26494f8941b', 3),
+(8, '1212', '7c4a8d09ca3762af61e59520943dc26494f8941b', 3);
 
 --
 -- Indexes for dumped tables
@@ -160,6 +261,12 @@ INSERT INTO `user` (`user_id`, `userName`, `password`, `role_i`) VALUES
 -- Indexes for table `bill`
 --
 ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `colloeg_dep`
+--
+ALTER TABLE `colloeg_dep`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -180,6 +287,12 @@ ALTER TABLE `departemnt`
 --
 ALTER TABLE `info`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `pdf`
+--
+ALTER TABLE `pdf`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `role`
@@ -208,19 +321,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `colloeg_dep`
+--
+ALTER TABLE `colloeg_dep`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `departemnt`
 --
 ALTER TABLE `departemnt`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pdf`
+--
+ALTER TABLE `pdf`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `role`
@@ -238,7 +363,7 @@ ALTER TABLE `type_contc`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
